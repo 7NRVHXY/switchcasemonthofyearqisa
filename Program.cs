55c37572af7e08a806a5a7de7c008a286075691e
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-11-01 00:43:39 +0400

    created8

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace switchcasemonthofyearqisa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthOfYear = 2;
+            switch (monthOfYear)
+            {
+                case 1: case 11: case 12:
+                    Console.WriteLine("winter");
+                    break;
+                case 2: case 3: case 4:
+                    Console.WriteLine("spring");
+                    break;
+                case 5: case 6: case 7:
+                    Console.WriteLine("summer");
+                    break;
+                case 8: case 9: case 10:
+                    Console.WriteLine("autumn");
+                    break;
+
+                default : Console.WriteLine("wrong");
+                    break;
+
+            }
+        }
+    }
+}
